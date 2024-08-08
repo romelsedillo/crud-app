375410b3e24bfe1e6b0dfac68955e5a60c697197
romelsedillo <romelsedillo@gmail.com>
2024-08-08 11:58:32 +0800

    Update appwrite.ts

--- src/appwrite/appwrite.ts
+++ src/appwrite/appwrite.ts
@@ -8,10 +8,3 @@ client
 
 export const account = new Account(client);
 export { ID } from "appwrite";
-
-// import { Client } from 'appwrite';
-// const client = new Client();
-
-// client
-//     .setEndpoint('https://cloud.appwrite.io/v1')
-//     .setProject('66aee777000efd6f8fdc');
